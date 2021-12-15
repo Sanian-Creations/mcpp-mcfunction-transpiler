bccb3c5dc6dbb0a74336f91c7917f63905c993ef
Sanian-Creations <sanian.creations@gmail.com>
2021-12-15 20:52:34 +0100

    string_match now uses memcmp
    memcmp is actually much faster than what I was doing, and my method could also cause memory alignment issues which might slow the whole thing down.
    memory alignment explained: https://developer.ibm.com/articles/pa-dalign/

--- src/simpleTypes.c
+++ src/simpleTypes.c
@@ -14,21 +14,5 @@ void print_string(string_T* string) {
 bool string_match(string_T* a, string_T* b) {
   if (a->len != b->len) return false;
 
-  // Gonna compare the strings 8 bytes at a time for EFFICIENCY
-  // I think. I vaguely remember that doing something like this
-  // is better than doing it byte by byte. Maybe.
-  size_t bigsteps = a->len / sizeof(long long);
-  size_t smallsteps = a->len % sizeof(long long);
-  
-  for (size_t i = 0; i < bigsteps; i++) {
-    if (((long long*)a->chars)[i] != ((long long*)b->chars)[i])
-      return false;
-  }
-  
-  for (size_t i = a->len - smallsteps; i < a->len; i++) {
-    if (a->chars[i] != b->chars[i])
-      return false;
-  }
-  
-  return true;
+  return memcmp(a->chars, b->chars, a->len) == 0;
 }
